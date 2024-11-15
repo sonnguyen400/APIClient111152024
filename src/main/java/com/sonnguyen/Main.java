8d333822980474df6f0d0eb3e2f47b87a2a3d51d
Nguyen Hoang Son <sonnguyen9616@gmail.com>
2024-11-15 14:15:59 +0700

    feat(TextFileUtils): save retrieved data to text file

--- src/main/java/com/sonnguyen/Main.java
+++ src/main/java/com/sonnguyen/Main.java
@@ -1,8 +1,17 @@
 package com.sonnguyen;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.sonnguyen.service.AbstractAlbumService;
+import com.sonnguyen.service.HttpClientAlbumService;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        AbstractAlbumService abstractAlbumService=new HttpClientAlbumService();
+        try {
+            abstractAlbumService.findAll().forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
